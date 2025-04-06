ba30d1c677104beb24946deb99d709afb6d5e6cf
Xinc111 <xin.chen2@estudiantat.upc.edu>
2025-04-06 21:00:22 +0200

    ui

--- Assets/Script/Script_menu.cs
+++ Assets/Script/Script_menu.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Script_menu : MonoBehaviour
 {
+    public GameObject pauseUI;
     public void EmpezarNivel(string NombreNivel)
     {
         SceneManager.LoadScene(NombreNivel);
     }
+
+    public void OnRestartPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnGameExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnGamePausePress() {
+        bool isPaused = !pauseUI.activeSelf;
+        pauseUI.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
 }
